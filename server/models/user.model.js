7f30818616fa2aae9cfdaf6ce492f7f6a6903656
Blaise Bakundukize <blaisebakundukize@gmail.com>
2020-08-20 11:59:03 +0200

    added about to user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -14,6 +14,10 @@ const UserSchema = new mongoose.Schema({
     match: [/.+\@.+\..+/, "Please fill a valid email address"],
     require: "Email is required",
   },
+  about: {
+    type: String,
+    trim: true,
+  },
   created: {
     type: Date,
     default: Date.now,
