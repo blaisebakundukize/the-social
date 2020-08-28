78c7975da2e261d9c46e5700113e5ea2598a86ae
Blaise Bakundukize <blaisebakundukize@gmail.com>
2020-08-28 09:07:37 +0200

    created newsfeed component

--- client/post/api-post.js
+++ client/post/api-post.js
@@ -64,4 +64,21 @@ const unlike = async (params, credentials, postId) => {
   }
 };
 
-export { create, remove, like, unlike };
+const listNewsFeed = async (params, credentials, signal) => {
+  try {
+    let response = await fetch("/api/posts/feed/" + params.userId, {
+      method: "GET",
+      signal: signal,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export { create, remove, like, unlike, listNewsFeed };
